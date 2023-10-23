9be370615c244acc3e79ed8451b6d29f33e5079d
Elijah Oquendo <elijahoquendo420@gmail.com>
2023-10-23 18:30:01 -0400

    Array filter method

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution here
+function findMatching(drivers, string) {
+    // Use the filter() method to iterate over the drivers array
+    // and return only the drivers whose names match the provided string
+    const matchingDrivers = drivers.filter(driver =>
+      driver.toLowerCase() === string.toLowerCase()
+    );
+  
+    return matchingDrivers;
+  }
+ 
+function fuzzyMatch(drivers, string) {
+    // Use the filter() method to iterate over the drivers array
+    // and return only the drivers whose names start with the provided string
+    const matchedDrivers = drivers.filter(driver =>
+      driver.toLowerCase().startsWith(string.toLowerCase())
+    );
+  
+    return matchedDrivers;
+  }
+  
+  const matchedDrivers = fuzzyMatch(drivers, 'D');
+  console.log(matchedDrivers);
+
+  function matchName(drivers, string) {
+    const matchedDrivers = drivers.filter(driver => driver.name.toLowerCase() === string.toLowerCase());
+    return matchedDrivers;
+  }
